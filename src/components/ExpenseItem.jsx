4fe653a1b045c2a0174a2147bf8ca42d3e8f60f3
Ritik Singh <webofritik@gmail.com>
2022-12-26 06:41:54 -0500

    react

--- src/components/ExpenseItem.jsx
+++ src/components/ExpenseItem.jsx
@@ -6,17 +6,21 @@ const Home = (props) => {
   const year = props.date.getFullYear();
   return (
     <>
-      <section className="flex justify-between bg-blue-100 p-5 m-5 rounded-xl shadow-lg">
-        <div className="flex space-x-6 items-center font-medium">
-          <div className="text-blue-500">
-            <div>{month}</div>
-            <div>{year}</div>
-            <div>{day}</div>
+      <section className="flex justify-center items-center ">
+        <div className="flex justify-between bg-blue-100 p-5  rounded-xl shadow-lg items-center w-6/12  mt-6">
+          <div className=" flex font-medium items-center space-x-6">
+            <div className=" text-center w-32  bg-indigo-400  py-3 rounded-xl text-white">
+              <div>{month}</div>
+              <div>{year}</div>
+              <div>{day}</div>
+            </div>
+            <h2 className="text-blue-600 text-xl font-semibold my-auto">
+              {props.title}
+            </h2>
           </div>
-          <h2 className="text-blue-600 text-xl font-semibold">{props.title}</h2>
-        </div>
-        <div className="py-3 px-8 bg-blue-200 text-blue-600 font-bold rounded-lg border-blue-600 border-2 hover:bg-blue-600 hover:text-white">
-          ${props.amount}
+          <div className="transition duration-150 ease-out hover:ease-in py-3 px-8 text-blue-600 font-bold rounded-lg border-blue-500 border-2 hover:bg-blue-500 hover:text-white">
+            ${props.amount}
+          </div>
         </div>
       </section>
     </>
